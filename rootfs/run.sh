bf601aed7eb8aeb290905fb5723bdc8958887591
Richard Sperry <richard@sperryfamily.net>
2025-11-03 18:31:31 -0500

    ssl

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -6,21 +6,22 @@ readonly cups_daemon_cfg=cupsd.conf
 
 function run() {
     bashio::log info "Entered Run.sh"
-    update_cups
+    update_cups_conf
 }
 
 # The HA API is not available from S6
-function update_cups() {
+function update_cups_conf() {
     # Get all possible host-names from configuration
     result=$(bashio::api.supervisor GET /core/api/config true || true)
     internal=$(bashio::jq "$result" '.internal_url' | cut -d'/' -f3 | cut -d':' -f1)
+    bashio::log.info "HA Int: $internal"
 
     if grep "$internal" "$real_cups_path/$cups_daemon_cfg"; then
         sed -i "s/^.*ServerAlias\a\  ${internal}" "$real_cups_path/$cups_daemon_cfg" # update config
-        s6-svc -r /etc/s6-overlay/s6-rc.d/cups-server                                # restart the service
+        bashio::log.info "Restarting CUPS after adding HA Internal domain"
+        s6-svc -r /etc/s6-overlay/s6-rc.d/cups-server # restart the service
     fi
 }
 
-run
-# Keep Running
-tail -f /dev/null
+run               # run entrypoint
+tail -f /dev/null # Keep Running
