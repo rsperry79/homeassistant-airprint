382971b5ba109f924dd63066c5a79333dbedcaa2
Richard Sperry <richard@sperryfamily.net>
2025-11-03 05:50:55 -0500

    ssl

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -3,10 +3,4 @@
 ulimit -n 1048576
 
 # Get all possible hostnames from configuration
-result=$(bashio::api.supervisor GET /core/api/config true || true)
-internal=$(bashio::jq "$result" '.internal_url' | cut -d'/' -f3 | cut -d':' -f1)
-external=$(bashio::jq "$result" '.external_url' | cut -d'/' -f3 | cut -d':' -f1)
-
-bashio::log.info "result $result\r\n"
-bashio::log.info "Int $internal\r\n"
-bashio::log.info "Ext $external"
+exec /bin/bash
