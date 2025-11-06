29ea4ad63d5ecd10afc4e5960f68359cae7e0caa
Richard Sperry <richard@sperryfamily.net>
2025-11-06 06:15:27 -0500

    update custom package

--- rootfs/opt/cups/cups-ssl-helpers.sh
+++ rootfs/opt/cups/cups-ssl-helpers.sh
@@ -45,8 +45,9 @@ function setup_ssl_private() {
     if [ ! -e "$_privkey" ]; then
         bashio::log.notice "SSL Private key does not exist at given path"
     else
+        echo
         # convert_private_key "$_privkey" "$output_file"
-        cp "$_privkey" "$CUPS_PRIVATE_KEY"
+        # cp "$_privkey" "$CUPS_PRIVATE_KEY"
     fi
 }
 
@@ -67,7 +68,7 @@ function setup_ssl_public() {
     else
         update_hosts "$_pubkey"
 
-        cp "$_pubkey" "$CUPS_PUBLIC_KEY"
+        #cp "$_pubkey" "$CUPS_PUBLIC_KEY"
         #convert_public_key "$_pubkey" "$output_file"
     fi
 }
