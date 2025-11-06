7c2575b67f53dd77a537634267cfe2618fc2cdbb
Richard Sperry <richard@sperryfamily.net>
2025-11-06 02:21:49 -0500

    update custom package

--- rootfs/opt/avahi/avahi-config.sh
+++ rootfs/opt/avahi/avahi-config.sh
@@ -4,16 +4,15 @@
 # shellcheck source="../common/paths.sh"
 source "/opt/common/paths.sh"
 
-
 function update_interfaces() {
     local bcast_interfaces=${1}
-    sed -i "s/^.*allow-interfaces=.*/allow-interfaces=${bcast_interfaces}/" "$real_avahi_path/$avahi_daemon_cfg"
+    sed -i "s/^.*allow-interfaces=.*/allow-interfaces=${bcast_interfaces}/" "$avahi_config_path/$avahi_daemon_cfg"
 }
 
 function update_reflectors() {
     local avahi_reflector=${1}
     local avahi_reflect_ipv=${2}
 
-    sed -i "s/^.*enable\-reflector=.*/enable\-reflector\=${avahi_reflector}/" "$real_avahi_path/$avahi_daemon_cfg"
-    sed -i "s/^.*reflect\-ipv=.*/reflect\-ipv\=${avahi_reflect_ipv}/" "$real_avahi_path/$avahi_daemon_cfg"
+    sed -i "s/^.*enable\-reflector=.*/enable\-reflector\=${avahi_reflector}/" "$avahi_config_path/$avahi_daemon_cfg"
+    sed -i "s/^.*reflect\-ipv=.*/reflect\-ipv\=${avahi_reflect_ipv}/" "$avahi_config_path/$avahi_daemon_cfg"
 }
