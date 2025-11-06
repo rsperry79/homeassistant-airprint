8d4a8cf35a867b053d4ab6aa9ba40f0b5a3e7770
Richard Sperry <richard@sperryfamily.net>
2025-11-06 00:19:19 -0500

    update custom package

--- rootfs/usr/templates/scripts/custom-install.sh
+++ rootfs/usr/templates/scripts/custom-install.sh
@@ -1,5 +1,6 @@
 #!/command/with-contend bashio
 # This script is ran on boot before avahi and cups.
+readonly package_dir=/config/packages
 
 function run() {
     bashio::log.info "Running Custom install script"
@@ -12,18 +13,18 @@ function install() {
     mkdir -p /var/spool/lpd/
     # check if dl'd to speed up boot
     if [ ! -e mfc9970cdwlpr-1.1.1-5.i386.deb ]; then
-        wget https://download.brother.com/welcome/dlf006526/mfc9970cdwlpr-1.1.1-5.i386.deb -O /config/packages/mfc9970cdwlpr-1.1.1-5.i386.deb
+        wget https://download.brother.com/welcome/dlf006526/mfc9970cdwlpr-1.1.1-5.i386.deb -O "$package_dir"/mfc9970cdwlpr-1.1.1-5.i386.deb
     fi
     # second check to ensure it was dl'd before install to prevent fatal errors
-    if [ -e /config/packages/mfc9970cdwlpr-1.1.1-5.i386.deb ]; then
+    if [ -e "$package_dir"/mfc9970cdwlpr-1.1.1-5.i386.deb ]; then
         # force all as brother only supplies a 386 package.
-        dpkg -i --force-all /config/packages/mfc9970cdwlpr-1.1.1-5.i386.deb
+        dpkg -i --force-all "$package_dir"/mfc9970cdwlpr-1.1.1-5.i386.deb
     fi
 
     if [ ! -e mfc9970cdwcupswrapper-1.1.1-5.i386.deb ]; then
-        wget https://download.brother.com/welcome/dlf006528/mfc9970cdwcupswrapper-1.1.1-5.i386.deb -O /config/packages/mfc9970cdwcupswrapper-1.1.1-5.i386.deb
+        wget https://download.brother.com/welcome/dlf006528/mfc9970cdwcupswrapper-1.1.1-5.i386.deb -O "$package_dir"/mfc9970cdwcupswrapper-1.1.1-5.i386.deb
     fi
-    if [ -e /config/packages/mfc9970cdwcupswrapper-1.1.1-5.i386.deb ]; then
+    if [ -e "$package_dir"/mfc9970cdwcupswrapper-1.1.1-5.i386.deb ]; then
         dpkg -i --force-all /config/packages/mfc9970cdwcupswrapper-1.1.1-5.i386.deb
     else
         bashio::log.notice "Cups wrapper was not present"
