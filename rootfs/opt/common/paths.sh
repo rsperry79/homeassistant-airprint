bf727519a1346a431cf846718d8930d3035d8ecd
Richard Sperry <richard@sperryfamily.net>
2025-11-06 02:07:58 -0500

    update custom package

--- rootfs/opt/common/paths.sh
+++ rootfs/opt/common/paths.sh
@@ -15,8 +15,8 @@ export real_cups_path=$real_config_path/cups
 export cups_log_path=$real_cups_path/logs
 export cups_ssl_path=$real_cups_path/ssl
 export cups_templates_path=$real_cups_path/templates
-
 export src_cups_templates_path=$templates_path/cups
+
 export cups_client_cfg=client.conf.tempio
 export cups_daemon_cfg=cupsd.conf.tempio
 export cups_files_cfg=cups-files.conf.tempio
@@ -28,8 +28,8 @@ export cups_files_cfg=cups-files.conf
 # Avahi
 export avahi_config_path=$real_config_path/avahi
 export avahi_services_path=$real_config_path/avahi/services
-export avahi_templates_path=$templates_path/avahi
-export src_avahi_templates_path=$src_avahi_templates_path/avahi
+export avahi_templates_path=$avahi_config_path/avahi
+export src_avahi_templates_path=$avahi_templates_path/avahi
 
 export avahi_daemon_cfg=avahi-daemon.conf.tempio
 export avahi_daemon_cfg=avahi-daemon.conf
