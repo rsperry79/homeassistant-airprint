07d7c238a88a8e667fbf0740562932f0374930a9
Richard Sperry <richard@sperryfamily.net>
2025-12-05 16:06:26 -0500

    update custom package

--- src/nginx/nginx-config.sh
+++ src/nginx/nginx-config.sh
@@ -54,7 +54,15 @@ function setup() {
         --arg nginx_access_log_location "$nginx_access_log_location" \
         --arg nginx_ssl_certificate "$nginx_ssl_certificate" \
         --arg nginx_ssl_key "$nginx_ssl_key" \
-        '{host_name: $host_name, docker_host_ip: $docker_host_ip, nginx_log_location: $nginx_log_location, nginx_log_level: $nginx_log_level, nginx_access_log_location: $nginx_access_log_location, nginx_ssl_certificate: $nginx_ssl_certificate, nginx_ssl_key: $nginx_ssl_key}' \
+        '{
+            host_name: $host_name,
+            docker_host_ip: $docker_host_ip,
+            nginx_log_location: $nginx_log_location,
+            nginx_log_level: $nginx_log_level,
+            nginx_access_log_location: $nginx_access_log_location,
+            nginx_ssl_certificate: $nginx_ssl_certificate,
+            nginx_ssl_key: $nginx_ssl_key
+        }' \
         /data/options.json)
 }
 
