7b96aef4b38929456027dcc42780b33a2fb19487
Richard Sperry <richard@sperryfamily.net>
2025-12-05 15:56:31 -0500

    update custom package

--- src/common/nginx-ip-helper.sh
+++ src/common/nginx-ip-helper.sh
@@ -3,5 +3,5 @@
 source "/opt/common/paths.sh"
 
 # shellcheck disable=SC2181
-echo "listen $(ip -o -4 a s docker0 | awk '{ print $4 }' | cut -d/ -f1):$nginx_port;" >/config/nginx/docker_ip
-echo "listen $(ip -o -4 a s hassio | awk '{ print $4 }' | cut -d/ -f1):$nginx_port;" >/config/nginx/hassio_ip
+echo "listen $(ip -o -4 a s docker0 | awk '{ print $4 }' | cut -d/ -f1):$nginx_port;" | tee /config/nginx/docker_ip
+echo "listen $(ip -o -4 a s hassio | awk '{ print $4 }' | cut -d/ -f1):$nginx_port;" | tee /config/nginx/hassio_ip
