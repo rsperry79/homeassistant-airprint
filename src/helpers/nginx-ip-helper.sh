646bad775931bb64a68fddda29fe1166ef2a3f3b
Richard Sperry <richard@sperryfamily.net>
2025-12-11 06:32:37 -0500

    update cups www

--- src/helpers/nginx-ip-helper.sh
+++ src/helpers/nginx-ip-helper.sh
@@ -1,7 +1,10 @@
 #!/command/with-contend bashio
-# shellcheck source="../common/settings.sh"
-source "/opt/common/settings.sh"
-
 # shellcheck disable=SC2181
-echo "listen $(ip -o -4 a s docker0 | awk '{ print $4 }' | cut -d/ -f1):$nginx_port;" | tee /config/nginx/docker_ip
-echo "listen $(ip -o -4 a s hassio | awk '{ print $4 }' | cut -d/ -f1):$nginx_port;" | tee /config/nginx/hassio_ip
+
+# # shellcheck source="../common/settings.sh"
+# source "/opt/common/settings.sh"
+
+ingress_port=$(bashio::addon.ingress_port)
+
+echo "listen $(ip -o -4 a s docker0 | awk '{ print $4 }' | cut -d/ -f1):$ingress_port;" | tee /config/nginx/docker_ip
+echo "listen $(ip -o -4 a s hassio | awk '{ print $4 }' | cut -d/ -f1):$ingress_port;" | tee /config/nginx/hassio_ip
