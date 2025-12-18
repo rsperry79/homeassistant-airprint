b4409552ba7059ee3ef945ba9373794aafcccd93
Richard Sperry <richard@sperryfamily.net>
2025-12-18 04:46:21 -0500

    update

--- src/avahi/avahi-config.sh
+++ src/avahi/avahi-config.sh
@@ -3,9 +3,6 @@
 # shellcheck source="../common/paths/avahi-paths.sh"
 source "/opt/common/paths/avahi-paths.sh"
 
-# shellcheck source="../common/settings/avahi-settings.sh"
-source "/opt/common/settings/avahi-settings.sh"
-
 # shellcheck source="./helpers/avahi-config-helpers.sh"
 source "/opt/avahi/helpers/avahi-config-helpers.sh"
 
@@ -30,54 +27,6 @@ function run() {
     fi
 }
 
-function setup_reflector () {
-    if bashio::config.has_value 'AVAHI_SETTINGS.AVAHI_REFLECTOR'; then
-        AVAHI_REFLECTOR_FLAG=$(bashio::config 'AVAHI_SETTINGS.AVAHI_REFLECTOR')
-
-    else
-        AVAHI_REFLECTOR_FLAG=$AVAHI_DEFAULT_REFLECTOR
-    fi
-
-    if [ "$AVAHI_REFLECTOR_FLAG" == true ]; then
-        AVAHI_REFLECTOR="yes"
-    else
-        AVAHI_REFLECTOR="no"
-    fi
-
-    export AVAHI_REFLECTOR
-}
-
-function setup_ipv_reflector () {
-    if bashio::config.has_value 'AVAHI_SETTINGS.AVAHI_REFLECTOR_IPV'; then
-        AVAHI_REFLECTOR_FLAG=$(bashio::config 'AVAHI_SETTINGS.AVAHI_REFLECTOR_IPV')
-    else
-        AVAHI_REFLECTOR_FLAG=$AVAHI_DEFAULT_REFLECT_IPV
-    fi
-
-    if [ "$AVAHI_REFLECTOR_FLAG" == true ]; then
-        AVAHI_REFLECTOR_IPV="yes"
-    else
-        AVAHI_REFLECTOR_IPV="no"
-    fi
-
-    export AVAHI_REFLECTOR_IPV
-}
-function setup_ipv6 () {
-    if bashio::config.has_value 'AVAHI_SETTINGS.AVAHI_USE_IPV6'; then
-        ipv6_flag=$(bashio::config 'AVAHI_SETTINGS.AVAHI_USE_IPV6')
-    else
-        ipv6_flag=$AVAHI_DEFAULT_USE_IPV6
-    fi
-
-    if [ "$ipv6_flag" == true ]; then
-        AVAHI_USE_IPV6="yes"
-    else
-        AVAHI_USE_IPV6="no"
-    fi
-
-    export AVAHI_USE_IPV6
-}
-
 function setup() {
     host_name=$(bashio::info.hostname)
     host="${host_name%%.*}"
@@ -89,13 +38,11 @@ function setup() {
 
 # Uses the template to regenerate the configuration file. Ensures a clean file.
 function autoconf_config() {
-    # Fill config file templates with runtime data
-
     config=$(jq \
         --arg host_name "$host" \
         --arg AVAHI_USE_IPV6 "$AVAHI_USE_IPV6" \
         --arg AVAHI_REFLECTOR "$AVAHI_REFLECTOR_FLAG" \
-        --arg AVAHI_REFLECTOR_IPV "$AVAHI_REFLECTOR_IPV_FLAG" \
+        --arg AVAHI_REFLECTOR_IPV "$AVAHI_REFLECTOR_IPV" \
         --arg bcast_interfaces "$bcast_interfaces" \
         '{
             host_name: $host_name,
--- src/avahi/helpers/avahi-config-helpers.sh
+++ src/avahi/helpers/avahi-config-helpers.sh
@@ -1,5 +1,7 @@
 #!/command/with-contend bashio
 # shellcheck disable=SC2181
+# shellcheck source="../../common/settings/avahi-settings.sh"
+source "/opt/common/settings/avahi-settings.sh"
 
 # shellcheck source="../../common/paths/avahi-paths.sh"
 source "/opt/common/paths/avahi-paths.sh"
@@ -15,4 +17,56 @@ function update_reflectors() {
 
     sed -i "s/^.*enable\-reflector=.*/enable\-reflector\=${AVAHI_REFLECTOR}/" "$avahi_config_path/$avahi_daemon"
     sed -i "s/^.*reflect\-ipv=.*/reflect\-ipv\=${AVAHI_REFLECTOR_IPV}/" "$avahi_config_path/$avahi_daemon"
+}
+
+
+
+
+function setup_reflector () {
+    if bashio::config.has_value 'AVAHI_SETTINGS.AVAHI_REFLECTOR'; then
+        AVAHI_REFLECTOR_FLAG=$(bashio::config 'AVAHI_SETTINGS.AVAHI_REFLECTOR')
+
+    else
+        AVAHI_REFLECTOR_FLAG=$AVAHI_DEFAULT_REFLECTOR
+    fi
+
+    if [ "$AVAHI_REFLECTOR_FLAG" == true ]; then
+        AVAHI_REFLECTOR="yes"
+    else
+        AVAHI_REFLECTOR="no"
+    fi
+
+    export AVAHI_REFLECTOR
+}
+
+function setup_ipv_reflector () {
+    if bashio::config.has_value 'AVAHI_SETTINGS.AVAHI_REFLECTOR_IPV'; then
+        AVAHI_REFLECTOR_FLAG=$(bashio::config 'AVAHI_SETTINGS.AVAHI_REFLECTOR_IPV')
+    else
+        AVAHI_REFLECTOR_FLAG=$AVAHI_DEFAULT_REFLECT_IPV
+    fi
+
+    if [ "$AVAHI_REFLECTOR_FLAG" == true ]; then
+        AVAHI_REFLECTOR_IPV="yes"
+    else
+        AVAHI_REFLECTOR_IPV="no"
+    fi
+
+    export AVAHI_REFLECTOR_IPV
+}
+
+function setup_ipv6 () {
+    if bashio::config.has_value 'AVAHI_SETTINGS.AVAHI_USE_IPV6'; then
+        ipv6_flag=$(bashio::config 'AVAHI_SETTINGS.AVAHI_USE_IPV6')
+    else
+        ipv6_flag=$AVAHI_DEFAULT_USE_IPV6
+    fi
+
+    if [ "$ipv6_flag" == true ]; then
+        AVAHI_USE_IPV6="yes"
+    else
+        AVAHI_USE_IPV6="no"
+    fi
+
+    export AVAHI_USE_IPV6
 }
