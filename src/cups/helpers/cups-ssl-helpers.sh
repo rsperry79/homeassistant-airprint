7738a5742711fcb92a8dbde9fe01414da1e2752c
Richard Sperry <richard@sperryfamily.net>
2025-12-19 16:57:06 -0500

    update

--- src/cups/helpers/cups-ssl-helpers.sh
+++ src/cups/helpers/cups-ssl-helpers.sh
@@ -20,23 +20,31 @@ function load_sources () {
     source "/opt/cups/helpers/cups-host-helpers.sh"
 }
 
+function lint () {
+    # shellcheck source="../../../lint/cups-settings.lint"
+    source "../../../lint/cups-settings.lint"
+}
+
+
 function setup_ssl() {
     load_sources || {
         bashio::log.error "Failed to load required sources"
         return 1
     }
 
-     if [ "$CUPS_ENCRYPTION" = "Never" ]; then
+    if [ "$CUPS_ENCRYPTION" = "Never" ]; then
         disable_ssl_config
     else
-        if [ "$CUPS_SELF_SIGN" == false ]; then
+        if [ "$CUPS_SELF_SIGN" == "false" ]; then
             bashio::log.info "Self sign is off"
-            rm -f "$cups_ssl_path/*"
+            if [ -d "$cups_ssl_path" ]; then
+                rm -f "$cups_ssl_path/*"
+            fi
             setup_ssl_public
             setup_ssl_private
         fi
-        # Second check for failure above
-        if [ "$CUPS_SELF_SIGN" == true ]; then
+        if [ "$CUPS_SELF_SIGN" == "true" ]; then
+            bashio::log.info "Using self-signed certificate"
             CUPS_HOST_ALIAS="$(hostname -f)"
         fi
     fi
@@ -60,7 +68,7 @@ function setup_ssl_private() {
         bashio::log.notice "SSL Private key does not exist at given path"
     else
         convert_private_key "$_privkey" "$CUPS_PRIVATE_KEY"
-     fi
+    fi
 }
 
 function setup_ssl_public() {
